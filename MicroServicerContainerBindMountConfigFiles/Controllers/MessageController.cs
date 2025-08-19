a85e6952f707cb0b0a06096a3b4caaf32830ae80
Sriram <sriram.openwave@gmail.com>
2025-08-19 23:14:06 +0530

    Add project files.

---
+++ MicroServicerContainerBindMountConfigFiles/Controllers/MessageController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace MicroServicerContainerBindMountConfigFiles.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MessageController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public MessageController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var message = _configuration["AppSettings:Message"] ?? "No message found";
+            return Ok(new { Message = message });
+        }
+    }
+}
